9d5c5352cab05888d66a4a61fb20a6f8fa784e25
enchance <enchance@gmail.com>
2021-06-29 15:16:32 +0800

    Simplified header

--- src/templates/partials/headers.js
+++ src/templates/partials/headers.js
@@ -37,90 +37,81 @@ const DeviceSizes = () => {
 }
 
 
-const GuestHeader = ({theme}) => {
+const GuestHeader = () => {
     return (
-        <header id="header" className={theme}>
-            <div id="navbar">
-                <nav className="navbar navbar-expand-lg navbar-dark">
-                    <div className="container">
-                        <div className="d-flex justify-content-start">
-                            <Link to="/" className={'navbar-brand'}><img src={logo} alt="" width="30" height="24" /></Link>
-                            <NavLink className={'navbar-brand'} to={`${s.LOGIN_URL}`}>Login</NavLink>
-                            <button onClick={doit} className={'btn btn-primary d-lg-none'}>Test private</button>
-                        </div>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-<div className="collapse navbar-collapse justify-content-between" id="navbarNav">
-    <ul className="navbar-nav">
-        <li className={'nav-item'}><NavLink className={'nav-link'} to={'/'} exact>Home</NavLink></li>
-        <li className={'nav-item'}><NavLink className={'nav-link'} to={`${s.REGISTER_URL}`}>Register</NavLink></li>
-        <li className={'nav-item'}><NavLink className={'nav-link'} to={`${s.LOGIN_URL}`}>Login</NavLink></li>
-        <li className={'nav-item'}><NavLink className={'nav-link'} to={'/foo'}>Foo</NavLink></li>
-    </ul>
-    <ul className="navbar-nav">
-        <li className={'nav-item me-3 d-none d-lg-inline-block'}>
-            <button onClick={doit} className={'btn btn-primary w-100'}>Test private</button>
-        </li>
-        <li className={'nav-item navbar-text'}>Watevs</li>
-        <li className={'nav-item navbar-text'}><DeviceSizes /></li>
-    </ul>
-</div>
-                    </div>
-                </nav>
+        <div className="container">
+            <div className="d-flex justify-content-start">
+                <Link to="/" className={'navbar-brand'}><img src={logo} alt="" width="30" height="24" /></Link>
+                <NavLink className={'navbar-brand'} to={`${s.LOGIN_URL}`}>Login</NavLink>
+                <button onClick={doit} className={'btn btn-primary d-lg-none'}>Test private</button>
             </div>
-        </header>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse justify-content-between" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className={'nav-item'}><NavLink className={'nav-link'} to={'/'} exact>Home</NavLink></li>
+                    <li className={'nav-item'}><NavLink className={'nav-link'} to={`${s.REGISTER_URL}`}>Register</NavLink></li>
+                    <li className={'nav-item'}><NavLink className={'nav-link'} to={`${s.LOGIN_URL}`}>Login</NavLink></li>
+                    <li className={'nav-item'}><NavLink className={'nav-link'} to={'/foo'}>Foo</NavLink></li>
+                </ul>
+                <ul className="navbar-nav">
+                    <li className={'nav-item me-3 d-none d-lg-inline-block'}>
+                        <button onClick={doit} className={'btn btn-primary w-100'}>Test private</button>
+                    </li>
+                    <li className={'nav-item navbar-text'}>Watevs</li>
+                    <li className={'nav-item navbar-text'}><DeviceSizes /></li>
+                </ul>
+            </div>
+        </div>
     )
 }
 
 
-const UserHeader = ({theme}) => {
+const UserHeader = () => {
     const {auth: {display}} = useSelector(state => state)
     return (
-        <header id="header" className={theme}>
-            <div id="navbar">
-                <nav className="navbar navbar-expand-lg navbar-dark">
-                    <div className="container">
-                        <div className="d-flex justify-content-start">
-                            <Link to="/" className={'navbar-brand'}><img src={logo} alt="" width="30" height="24" /></Link>
-                            <NavLink className={'navbar-brand'} to={`${s.LOGOUT_URL}`}>Logout</NavLink>
-                            <button onClick={doit} className={'btn btn-primary d-lg-none'}>Test private</button>
-                        </div>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-<div className="navbar-collapse collapse justify-content-between" id="navbarNav">
-    <ul className="navbar-nav">
-        <li className={'nav-item'}><NavLink className={'nav-link'} to={'/'}>Home</NavLink></li>
-        <li className={'nav-item navbar-text'}>Profile</li>
-        <li className={'nav-item'}><NavLink className={'nav-link'} to={'/foo'}>Foo</NavLink></li>
-    </ul>
-    <ul className={'navbar-nav'}>
-        <li className={'nav-item me-3 d-none d-lg-inline-block'}>
-            <button onClick={doit} className={'btn btn-primary w-100'}>Test private</button>
-        </li>
-        <li className={'nav-item navbar-text display d-none d-lg-block'}><strong>Hi {display}!</strong></li>
-        <li className={'nav-item'}><NavLink className={'nav-link'} to={`${s.LOGOUT_URL}`}>Logout</NavLink></li>
-        <li className={'nav-item navbar-text d-none d-lg-inline-block'}><DeviceSizes /></li>
-    </ul>
-</div>
-                    </div>
-                </nav>
+        <div className="container">
+            <div className="d-flex justify-content-start">
+                <Link to="/" className={'navbar-brand'}><img src={logo} alt="" width="30" height="24" /></Link>
+                <NavLink className={'navbar-brand'} to={`${s.LOGOUT_URL}`}>Logout</NavLink>
+                <button onClick={doit} className={'btn btn-primary d-lg-none'}>Test private</button>
             </div>
-        </header>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="navbar-collapse collapse justify-content-between" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className={'nav-item'}><NavLink className={'nav-link'} to={'/'}>Home</NavLink></li>
+                    <li className={'nav-item navbar-text'}>Profile</li>
+                    <li className={'nav-item'}><NavLink className={'nav-link'} to={'/foo'}>Foo</NavLink></li>
+                </ul>
+                <ul className={'navbar-nav'}>
+                    <li className={'nav-item me-3 d-none d-lg-inline-block'}>
+                        <button onClick={doit} className={'btn btn-primary w-100'}>Test private</button>
+                    </li>
+                    <li className={'nav-item navbar-text display d-none d-lg-block'}><strong>Hi {display}!</strong></li>
+                    <li className={'nav-item'}><NavLink className={'nav-link'} to={`${s.LOGOUT_URL}`}>Logout</NavLink></li>
+                    <li className={'nav-item navbar-text d-none d-lg-inline-block'}><DeviceSizes /></li>
+                </ul>
+            </div>
+        </div>
     )
 }
 
 
 const Header = () => {
     const {auth, site} = useSelector(state => state)
-
     return (
-        <>
-            {auth.is_auth && <UserHeader theme={site.theme} /> || <GuestHeader theme={site.theme} />}
-        </>
+        <header id="header" className={site.theme}>
+            <div id="navbar">
+                <nav className="navbar navbar-expand-lg navbar-dark">
+                    {auth.is_auth && <UserHeader /> || <GuestHeader />}
+                </nav>
+            </div>
+        </header>
     )
 }
 
