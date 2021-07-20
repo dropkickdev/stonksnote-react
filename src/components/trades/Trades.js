208ba783e8165eccf1379d51f0a810833ae23ad4
enchance <enchance@gmail.com>
2021-07-20 19:08:11 +0800

    Style updates for table

--- src/components/trades/Trades.js
+++ src/components/trades/Trades.js
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react"
 import { useSelector } from "react-redux"
-import { NavLink, Link, useRouteMatch, useParams } from "react-router-dom"
+import { NavLink, Link, useRouteMatch, useParams, Switch, Route } from "react-router-dom"
 import { Field, Form, Formik } from "formik"
 import * as yup from 'yup'
 
-import { ScrollToStart, SearchForm } from "../../app/utilcomp"
+import { ScrollToStart, SearchForm, IF } from "../../app/utilcomp"
 import UserTemplate from "../../templates/layouts/UserTemplate"
 import { HeaderActions } from "../../templates/partials/partials"
 import { api_mark_create } from "./api_trades"
@@ -12,34 +12,52 @@ import { SimpleInputHTML } from "../../templates/partials/forms"
 
 
 export const TradeList = () => {
-    const [state, setState] = useState({currpage: 1})
+    const [state, setState] = useState({currpage: 1, data: []})
     const [search, setSearch] = useState('')
     const {tab} = useParams()
     const {site} = useSelector(state => state)
 
     useEffect(() => {
-        // Get data here
+        const data = [
+            {ticker: 'ABC', action: 'sell', shares: '12,000', buy: '', sell: '13.00', minsell: '12.6',
+                gainloss: '1,500', currency: 'PHP', total: '99,432.00', bought: '2021-03-06',
+                sold: '2021-03-06', id: 1},
+            {ticker: 'ABC', action: 'buy', shares: '12,000', buy: '12.00', sell: '', minsell: '12.6',
+                gainloss: '1,500', currency: 'PHP', total: '99,432.00', bought: '2021-03-06',
+                sold: '2021-03-06', id: 2},
+            {ticker: 'ABC', action: 'buy', shares: '12,000', buy: '12.00', sell: '', minsell: '12.6',
+                gainloss: '1,500', currency: 'PHP', total: '99,432.00', bought: '2021-03-06',
+                sold: '2021-03-06', id: 3},
+            {ticker: 'ABC', action: 'buy', shares: '12,000', buy: '12.00', sell: '', minsell: '12.6',
+                gainloss: '1,500', currency: 'PHP', total: '99,432.00', bought: '2021-03-06',
+                sold: '2021-03-06', id: 4},
+            {ticker: 'ABC', action: 'buy', shares: '12,000', buy: '12.00', sell: '', minsell: '12.6',
+                gainloss: '1,500', currency: 'PHP', total: '99,432.00', bought: '2021-03-06',
+                sold: '2021-03-06', id: 5},
+            {ticker: 'ABC', action: 'buy', shares: '12,000', buy: '12.00', sell: '', minsell: '12.6',
+                gainloss: '1,500', currency: 'PHP', total: '99,432.00', bought: '2021-03-06',
+                sold: '2021-03-06', id: 6},
+            {ticker: 'ABC', action: 'buy', shares: '12,000', buy: '12.00', sell: '', minsell: '12.6',
+                gainloss: '1,500', currency: 'PHP', total: '99,432.00', bought: '2021-03-06',
+                sold: '2021-03-06', id: 7},
+        ]
+        setState({...state, data})
     }, [])
 
-    // switch(tab) {
-    //     case 'active':
-    //         const title = <h1></h1>
-    // }
+    useEffect(() => {
+
+    }, [state])
 
     let title = ''
     let table_title = ''
     switch(tab) {
-        case 'active':
-            title = <h1>Trades: <span className={'nobold'}>Active</span></h1>
-            table_title = 'Currently bought stocks'
+        case 'buy':
+            title = <h1>Trades: <span className={'nobold'}>Buy</span></h1>
+            table_title = 'Buy trades'
             break
-        case 'gain':
-            title = <h1>Trades: <span className={'nobold'}>Won</span></h1>
-            table_title = 'Stocks you earned from'
-            break
-        case 'loss':
-            title = <h1>Trades: <span className={'nobold'}>Lost</span></h1>
-            table_title = 'Stocks you lost from'
+        case 'sell':
+            title = <h1>Trades: <span className={'nobold'}>Sell</span></h1>
+            table_title = 'Sell trades'
             break
         default:
             title = <h1>Trades</h1>
@@ -52,6 +70,59 @@ export const TradeList = () => {
         console.log(query)
     };
 
+    const tabHandler = (tabid=false) => {
+        console.log(tabid)
+        let data = null
+        if(tabid === 'buy') {
+            data = [
+                {ticker: 'ABC', action: 'buy', shares: '12,000', buy: '12.00', sell: '', minsell: '12.6',
+                    gainloss: '1,500', currency: 'PHP', total: '99,432.00', bought: '2021-03-06',
+                    sold: '2021-03-06', id: 1},
+                {ticker: 'ABC', action: 'buy', shares: '12,000', buy: '12.00', sell: '', minsell: '12.6',
+                    gainloss: '1,500', currency: 'PHP', total: '99,432.00', bought: '2021-03-06',
+                    sold: '2021-03-06', id: 2},
+            ]
+        }
+        else if(tabid === 'sell') {
+            data = [
+                {ticker: 'ABC', action: 'sell', shares: '12,000', buy: '', sell: '12.00', minsell: '12.6',
+                    gainloss: '1,500', currency: 'PHP', total: '99,432.00', bought: '2021-03-06',
+                    sold: '2021-03-06', id: 1},
+                {ticker: 'ABC', action: 'sell', shares: '12,000', buy: '', sell: '12.00', minsell: '12.6',
+                    gainloss: '1,500', currency: 'PHP', total: '99,432.00', bought: '2021-03-06',
+                    sold: '2021-03-06', id: 2},
+                {ticker: 'ABC', action: 'sell', shares: '12,000', buy: '', sell: '12.00', minsell: '12.6',
+                    gainloss: '1,500', currency: 'PHP', total: '99,432.00', bought: '2021-03-06',
+                    sold: '2021-03-06', id: 3},
+            ]
+        }
+        else {
+            data = [
+                {ticker: 'ABC', action: 'sell', shares: '12,000', buy: '', sell: '13.00', minsell: '12.6',
+                    gainloss: '1,500', currency: 'PHP', total: '99,432.00', bought: '2021-03-06',
+                    sold: '2021-03-06', id: 1},
+                {ticker: 'ABC', action: 'buy', shares: '12,000', buy: '12.00', sell: '', minsell: '12.6',
+                    gainloss: '1,500', currency: 'PHP', total: '99,432.00', bought: '2021-03-06',
+                    sold: '2021-03-06', id: 2},
+                {ticker: 'ABC', action: 'buy', shares: '12,000', buy: '12.00', sell: '', minsell: '12.6',
+                    gainloss: '1,500', currency: 'PHP', total: '99,432.00', bought: '2021-03-06',
+                    sold: '2021-03-06', id: 3},
+                {ticker: 'ABC', action: 'buy', shares: '12,000', buy: '12.00', sell: '', minsell: '12.6',
+                    gainloss: '1,500', currency: 'PHP', total: '99,432.00', bought: '2021-03-06',
+                    sold: '2021-03-06', id: 4},
+                {ticker: 'ABC', action: 'buy', shares: '12,000', buy: '12.00', sell: '', minsell: '12.6',
+                    gainloss: '1,500', currency: 'PHP', total: '99,432.00', bought: '2021-03-06',
+                    sold: '2021-03-06', id: 5},
+                {ticker: 'ABC', action: 'buy', shares: '12,000', buy: '12.00', sell: '', minsell: '12.6',
+                    gainloss: '1,500', currency: 'PHP', total: '99,432.00', bought: '2021-03-06',
+                    sold: '2021-03-06', id: 6},
+                {ticker: 'ABC', action: 'buy', shares: '12,000', buy: '12.00', sell: '', minsell: '12.6',
+                    gainloss: '1,500', currency: 'PHP', total: '99,432.00', bought: '2021-03-06',
+                    sold: '2021-03-06', id: 7},
+            ]
+        }
+        setState({...state, data})
+    }
 
     return (
         <UserTemplate page={'trades-page'}>
@@ -67,16 +138,13 @@ export const TradeList = () => {
                 <div className="page-options">
                     <ul className="nav nav-pills nav-fill">
                         <li className="nav-item">
-                            <NavLink to={`/trades`} className="nav-link" aria-current="page" exact>All Trades</NavLink>
+                            <NavLink to={`/trades`} onClick={() => tabHandler()} className="nav-link" aria-current="page" exact>All Trades</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink to={`/trades/active`} className="nav-link" aria-current="page">Active</NavLink>
+                            <NavLink to={`/trades/buy`} onClick={() => tabHandler('buy')} className="nav-link" aria-current="page">Buy trades</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink to={`/trades/gain`} className="nav-link" aria-current="page">Won</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to={`/trades/loss`} className="nav-link" aria-current="page">Lost</NavLink>
+                            <NavLink to={`/trades/sell`} onClick={() => tabHandler('sell')} className="nav-link" aria-current="page">Sell trades</NavLink>
                         </li>
                     </ul>
                 </div>
@@ -84,9 +152,20 @@ export const TradeList = () => {
                 <div className="listing mt-5">
                     <header className={'split-focus-left'}>
                         <h2>{table_title}</h2>
-                        <SearchForm callback={searchHandler} placeholder={'Symbol'} maxlength={6} />
+                        <SearchForm callback={searchHandler} placeholder={'Symbol'} maxLength={6} />
                     </header>
-                    <TradeTable />
+                    {state.data ? (
+                        <>
+                            <TradeTable data={state.data} />
+                        </>
+                    ) : (
+                        <div className="spinner">
+                            <div className="sk-cube1 sk-cube"></div>
+                            <div className="sk-cube2 sk-cube"></div>
+                            <div className="sk-cube4 sk-cube"></div>
+                            <div className="sk-cube3 sk-cube"></div>
+                        </div>
+                    )}
                 </div>
             </div>
             <footer id="footer">Footer</footer>
@@ -94,30 +173,7 @@ export const TradeList = () => {
     )
 }
 
-export const TradeTable = () => {
-    const data = [
-        {symbol: 'ABC', shares: '12,000', buy: '12.00', sell: '13.00', minsell: '12.6',
-            gainloss: '1,500', currency: 'PHP', total: '99,432.00', bought: '2021-03-06',
-            sold: '2021-03-06', id: 1},
-        {symbol: 'ABC', shares: '12,000', buy: '12.00', sell: '13.00', minsell: '12.6',
-            gainloss: '1,500', currency: 'PHP', total: '99,432.00', bought: '2021-03-06',
-            sold: '2021-03-06', id: 2},
-        {symbol: 'ABC', shares: '12,000', buy: '12.00', sell: '13.00', minsell: '12.6',
-            gainloss: '1,500', currency: 'PHP', total: '99,432.00', bought: '2021-03-06',
-            sold: '2021-03-06', id: 3},
-        {symbol: 'ABC', shares: '12,000', buy: '12.00', sell: '13.00', minsell: '12.6',
-            gainloss: '1,500', currency: 'PHP', total: '99,432.00', bought: '2021-03-06',
-            sold: '2021-03-06', id: 4},
-        {symbol: 'ABC', shares: '12,000', buy: '12.00', sell: '13.00', minsell: '12.6',
-            gainloss: '1,500', currency: 'PHP', total: '99,432.00', bought: '2021-03-06',
-            sold: '2021-03-06', id: 5},
-        {symbol: 'ABC', shares: '12,000', buy: '12.00', sell: '13.00', minsell: '12.6',
-            gainloss: '1,500', currency: 'PHP', total: '99,432.00', bought: '2021-03-06',
-            sold: '2021-03-06', id: 6},
-        {symbol: 'ABC', shares: '12,000', buy: '12.00', sell: '13.00', minsell: '12.6',
-            gainloss: '1,500', currency: 'PHP', total: '99,432.00', bought: '2021-03-06',
-            sold: '2021-03-06', id: 7},
-    ]
+export const TradeTable = ({data}) => {
     return (
         <table className="table table-striped table-hover table-boundless">
             <thead>
@@ -144,15 +200,17 @@ export const TradeTable = () => {
 };
 
 export const TradeEntry = props => {
-    const {symbol, shares, buy, sell, minsell, gainloss, currency, total,
-          bought, sold, id} = props
+    const {ticker, shares, buy, sell, minsell, gainloss, currency, total,
+          bought, sold, id, action} = props
+    const buyshade = action === 'buy' ? '' : 'block-out-buy'
+    const sellshade = action === 'buy' ? 'block-out-sell' : ''
     return (
         <>
             <tr>
-                <th scope={'row'} className={'symbol'}>{symbol}</th>
+                <th scope={'row'} className={'ticker'}>{ticker}</th>
                 <td className={'shares'}>{shares}</td>
-                <td className={'buy focus-buy text-bold clr-buy-darker'}>{buy}</td>
-                <td className={'sell focus-sell text-bold clr-sell-darker'}>{sell}</td>
+                <td className={`buy focus-buy text-bold clr-buy-darker ${buyshade}`}>{buy}</td>
+                <td className={`sell focus-sell text-bold clr-sell-darker ${sellshade}`}>{sell}</td>
                 <td className={'minsell text-bold'}>&gt; {minsell}</td>
                 <td className={'gainloss'}>{gainloss} <small className={'clr-light'}>{currency}</small></td>
                 <td className={'total'}>{total} <small className={'clr-light'}>{currency}</small></td>
@@ -170,12 +228,12 @@ export const TradeEntry = props => {
 
 export const MarkModal = () => {
     const init = {
-        symbol: '',
+        ticker: '',
         expires: ''
     }
 
     const schema = yup.object({
-        symbol: yup.string().max(10).required(),
+        ticker: yup.string().max(10).required(),
         expires: yup.string()
     })
 
@@ -206,7 +264,7 @@ export const MarkModal = () => {
 <Form>
     <h3>Add Mark</h3>
     <div className="mb-3">
-        <SimpleInputHTML name={'symbol'} placeholder={'Symbol name'} touched={touched} errors={errors} />
+        <SimpleInputHTML name={'ticker'} placeholder={'Ticker name'} touched={touched} errors={errors} />
     </div>
     <div className="submit">
         <button type={'submit'} className={'btn btn-primary btn-lg w-100'}>Submit</button>
